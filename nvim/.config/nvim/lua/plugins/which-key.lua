e45dba3072aa0529607789d8d501f3c1445f4899
Jake Rainis <jakerainis@gmail.com>
2025-07-29 14:56:43 -0400

    Updated dashboard, messing around with copilot.

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -4,7 +4,7 @@ return {
     "folke/which-key.nvim",
     opts = {
       spec = {
-        { "<leader>a", group = "Actions", icon = { icon = "⚡ ", color = "yellow" } },
+        { "<leader>a", group = "actions", icon = { icon = "⚡ ", color = "yellow" } },
       },
     },
   },
