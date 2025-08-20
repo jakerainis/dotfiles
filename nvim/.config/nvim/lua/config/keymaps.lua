444a69deefd2777f495ea8cf1fa2b2a6a3519d62
Jake Rainis <jakerainis@gmail.com>
2025-08-20 15:24:29 -0400

    Clean up bad keymaps, added tab indentation for blocks, updated markdown render config.

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -18,39 +18,20 @@ vim.keymap.set({ "n", "v" }, "<D-d>", "<C-n>", { desc = "Multi-cursor select nex
 -- Sort lines (cmd+shift+a)
 vim.keymap.set({ "n", "v" }, "<D-A>", ":sort<CR>", { desc = "Sort lines", remap = true })
 
+-- Tab indents current line
+vim.keymap.set("n", "<Tab>", ">>", { noremap = true })
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true })
+vim.keymap.set("i", "<Tab>", "<C-o>>>", { noremap = true })
+
 -- Shift+Tab unindents current line
 vim.keymap.set("n", "<S-Tab>", "<<", { noremap = true })
 vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true })
 vim.keymap.set("i", "<S-Tab>", "<C-o><<", { noremap = true })
 
--- MacOS MIRRORS
 -- Line/word deletion (cmd/alt+backspace)
 vim.keymap.set({ "i", "n", "v" }, "<A-BS>", "<C-w>", { desc = "Delete preceding word" })
 vim.keymap.set({ "i", "n", "v" }, "<D-BS>", "<C-u>", { desc = "Delete to beginning of line" })
--- Beginning/end line navigation (cmd+left/right)
-vim.keymap.set({ "n", "v" }, "<C-a>", "0", { desc = "Go to beginning of line" })
-vim.keymap.set({ "n", "v" }, "<C-e>", "$", { desc = "Go to end of line" })
-vim.keymap.set("i", "<C-a>", "<C-o>0", { desc = "Go to beginning of line" })
-vim.keymap.set("i", "<C-e>", "<C-o>$", { desc = "Go to end of line" })
--- Beginning/end line selection (shift+cmd+left/right)
-vim.keymap.set("n", "<D-S-Left>", "v0", { desc = "Select to beginning of line" })
-vim.keymap.set("n", "<D-S-Right>", "v$", { desc = "Select to end of line" })
-vim.keymap.set("v", "<D-S-Left>", "0", { desc = "Extend selection to beginning of line" })
-vim.keymap.set("v", "<D-S-Right>", "$", { desc = "Extend selection to end of line" })
-vim.keymap.set("i", "<D-S-Left>", "<Esc>v0", { desc = "Select to beginning of line" })
-vim.keymap.set("i", "<D-S-Right>", "<Esc>v$", { desc = "Select to end of line" })
--- Prev/next word navigation (alt+left/right)
-vim.keymap.set({ "n", "v" }, "<M-Left>", "b", { desc = "Go to preceding word" })
-vim.keymap.set({ "n", "v" }, "<M-f>", "w", { desc = "Go to next word" }) -- Use M-f instead of M-Right
-vim.keymap.set("i", "<M-Left>", "<C-o>b", { desc = "Go to preceding word" })
-vim.keymap.set("i", "<M-f>", "<C-o>w", { desc = "Go to next word" })
--- Prev/next word selection (shift+alt+left/right)
-vim.keymap.set("n", "<M-S-Left>", "vb", { desc = "Select to preceding word" })
-vim.keymap.set("n", "<M-S-Right>", "ve", { desc = "Select to next word end" })
-vim.keymap.set("v", "<M-S-Left>", "b", { desc = "Extend selection to preceding word" })
-vim.keymap.set("v", "<M-S-Right>", "e", { desc = "Extend selection to next word end" })
-vim.keymap.set("i", "<M-S-Left>", "<Esc>vb", { desc = "Select to preceding word" })
-vim.keymap.set("i", "<M-S-Right>", "<Esc>ve", { desc = "Select to next word end" })
+
 -- Surround with common pairs
 vim.keymap.set("v", '"', 'c""<Esc>P', { desc = "Surround with double quotes", nowait = true })
 vim.keymap.set("v", "'", "c''<Esc>P", { desc = "Surround with single quotes", nowait = true })
--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -3,6 +3,14 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
+      code = {
+        border = "none",
+        conceal_delimiters = false,
+        inline_left = "`",
+        inline_right = "`",
+        language_icon = true,
+        language_name = true,
+      },
       link = {
         enabled = false,
       },
